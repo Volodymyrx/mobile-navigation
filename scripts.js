fcf9d730267021d876dd4289667568a96a0707e0
Volodymyrx <volodiax@mail.ru>
2020-04-22 19:03:40 +0300

    finish mobile nav

---
+++ scripts.js
@@ -0,0 +1,24 @@
+console.log('i am here!')
+// burger in transform menu
+const menuToggle=document.querySelector('#menu-toggle')
+const mobileNav=document.querySelector('#mobile-nav')
+menuToggle.onclick=function(){
+  menuToggle.classList.toggle('menu-icon-active')
+  mobileNav.classList.toggle('mobile-nav__active')
+}
+
+
+// document.querySelector('.menu-icon').onclick= function(){document.querySelector('.menu-icon-line').classList.toggle('menu-icon-active')};
+
+
+
+
+
+
+
+
+
+
+
+
+
